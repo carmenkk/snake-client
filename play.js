c652b7007578f261829cbb1a4fe4f194f85a11ca
carmen kuang <carmenkuang87@gmail.com>
2022-02-17 03:42:37 +0000

    move setupinput

--- play.js
+++ play.js
@@ -1,23 +1,10 @@
 
-const { connected } = require("process");
-
-const connect= require('./client');
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data",handleUserInput);
-}
-  
-  
-  const handleUserInput = (key) => {
-    if (key === '\u0003') {
-    process.exit();
-    }
-  }
+setupInput();
+
   
