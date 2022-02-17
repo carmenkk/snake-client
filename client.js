95f1ff0b5658f8d99f4399f98d887a5cab53c499
carmen kuang <carmenkuang87@gmail.com>
2022-02-17 05:42:18 +0000

    port

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
+    IP,
+    PORT,
   });
   conn.setEncoding("utf8");
   conn.on("connect",() => {
@@ -21,5 +22,5 @@ const connect = function () {
 
 
 
-module.exports = connect;
+module.exports = {connect};
 
