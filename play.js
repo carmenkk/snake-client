801331485675bfb43dfc0b0dd574cd66deab67bd
carmen kuang <carmenkuang87@gmail.com>
2022-02-17 00:01:32 +0000

    remove connect function

--- play.js
+++ play.js
@@ -1,28 +1,11 @@
-const net = require("net");
+
 const { connected } = require("process");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("connected to the game.")
-  });
-  conn.on('data', (data) => {
-    console.log(data);
-   
-  });
-  return conn;
-  
-};
+const connect = require('./client');
 
 
 
 
-console.log("Connecting ...");
-connect();
+
+
+
