f5718f8cf019553fc3c398591914d136cfa8fceb
carmen kuang <carmenkuang87@gmail.com>
2022-02-17 05:19:07 +0000

    movement commands

--- play.js
+++ play.js
@@ -1,10 +1,10 @@
-
+const net = require("net");
 const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+
+setupInput(connect());
 
   
