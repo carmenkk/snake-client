58b6cca3bc9860de38ad98f420499ee2d988a3e6
carmen kuang <carmenkuang87@gmail.com>
2022-02-17 05:18:28 +0000

    movement commands

--- input.js
+++ input.js
@@ -1,10 +1,14 @@
+let connection;
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data",handleUserInput);
+  return stdin;
 }
   
   
@@ -12,6 +16,20 @@ const setupInput = function () {
     if (key === '\u0003') {
     process.exit();
     }
+    if (key ==="w") {
+      connection.write("Move: up")
+    }
+    if (key ==="a") {
+      connection.write("Move: left")
+    }
+    if (key ==="s") {
+      connection.write("Move: down")
+    }
+    if (key ==="d") {
+      connection.write("Move: right")
+    }
+    
   }
-
+  setupInput();
+ 
   module.exports = {setupInput};
